51185f2fec059a6b249a8f1cb56e4b2329ae1944
jose <you@example.com>
2026-06-17 14:07:45 -0400

    ejercicio en desarrollo

--- agenda de contactos.py
+++ agenda de contactos.py
@@ -22,3 +22,13 @@ for i,contacto in enumerate(agenda,start=1):
    {contacto[´email ]})
    print (f"total de contactos:{len(agenda)}/n")
 
+
+ def buscar_contacto[nombre]:
+    for contacto in agenda:
+        if contacto["nombre"].lower() == nombre.lower():
+           print(f"encontrado:{contacto[´ nombre ']} / tel:{contacto[´telefono´]} / email:
+   {contacto[´email ]})  ") 
+        return contacto
+    print(f"no se encontro ningun contacto con el nombre ´{nombre}´.")
+    return None
+
